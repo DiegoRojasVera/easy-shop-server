97335e9a2cfc8a8378f095e3e61e23fa9e5bd76a
Diego <diegorojasvera@gmail.com>
2021-07-29 16:06:11 -0400

    modificacion de error de heroku

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,9 +9,10 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
-            { url: /\/products\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]
@@ -19,11 +20,13 @@ function authJwt() {
 }
 
 async function isRevoked(req, payload, done) {
-    if (!payload.isAdmin) {
+    if(!payload.isAdmin) {
         done(null, true)
     }
 
     done();
 }
 
-module.exports = authJwt;
+
+
+module.exports = authJwt
