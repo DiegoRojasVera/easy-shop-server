4a096ff8bf8e25a8124bab4cf9c0110d478d9191
Diego <diegorojasvera@gmail.com>
2021-07-31 22:12:29 -0400

    Para solucionar problema en login

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -3,20 +3,20 @@ const expressJwt = require('express-jwt');
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-    return expressJwt({ secret, algorithms: ['HS256'], isRevoked })
-        .unless({
-            path: [
-                // public routes that don't require authentication
-                /\/public\/uploads(.*)/,
-                `${api}/users/login`,
-                `${api}/users/register`,
-                { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-                { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
-                { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'], },
-                { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },],
-
-                
-        });
+    return expressJwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path: [
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
+    })
 }
 
 async function isRevoked(req, payload, done) {
