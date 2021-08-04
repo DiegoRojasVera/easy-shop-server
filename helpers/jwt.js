d11ef8398e724a601adbb8c10d2f609eb2b6d193
Diego <diegorojasvera@gmail.com>
2021-08-04 16:03:46 -0400

    jwt para solucion de Admin

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -10,6 +10,7 @@ function authJwt() {
                 { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
                 { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
                 { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "POST", "OPTIONS"] },
+                { url: /\/api\/v1\/users\/.*/,methods: ['GET']},
 
                 `${api}/users/login`,
                 `${api}/users/signin`,
